c2cabed77a7506669289ff66a4226320008efaaa
VacatedSum <59126285+VacatedSum@users.noreply.github.com>
2020-04-09 09:44:09 -0400

    testHello now accepts name parameter!

--- src/main/java/TestHello.java
+++ src/main/java/TestHello.java
@@ -1,14 +1,16 @@
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class TestHello {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String Hello(String name) {
+	public String Hello(@DefaultValue("World") @QueryParam("name") String name) {
 		return "Hello " + name + "!";
 		
 	}
