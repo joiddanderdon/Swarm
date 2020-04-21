38118b063d26809da643127f843ed2d303e8c11e
VacatedSum <59126285+VacatedSum@users.noreply.github.com>
2020-04-21 08:54:23 -0400

    - Methods now communicating almost directly with database. - Still need to fix Tick() method - details in comments - Need a trigger for Tick() method.

--- src/main/java/ServerBoard.java
+++ src/main/java/ServerBoard.java
@@ -25,10 +25,10 @@ import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationExceptio
 
 @Path("/")
 public class ServerBoard {
-	private static final String dbConnectionUrl = "jdbc:mysql://localhost:3306/swarm";
+	private static final String dbConnectionUrl = "jdbc:mysql://localhost:3306/swarm?autoReconnect=true&useSSL=false";
 	private static final String dbConnectionUser = "root";
 	private static final String dbConnectionPass = "root";
-	private static final String connectString = dbConnectionUrl + "?user=" + dbConnectionUser + "&password=" + dbConnectionPass;
+	private static final String connectString = dbConnectionUrl + "&user=" + dbConnectionUser + "&password=" + dbConnectionPass;
 	//private static final String connectString = dbConnectionUrl  + "?useSSL=false&" + "?user=" + dbConnectionUser + "&password=" + dbConnectionPass;
 	private static Connection connection;
 	private static Statement statement;
@@ -44,7 +44,7 @@ public class ServerBoard {
 	//run the Stalk method on each Zombie contained within
 	private static ArrayList<Sprite> sprites;
 	
-	private int tickCount;
+	private int tickCount = 0;
 	
 	public ServerBoard() throws SQLException, ClassNotFoundException {
 		sprites = new ArrayList<Sprite>();
@@ -79,6 +79,33 @@ public class ServerBoard {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getBoard() {
+		sprites = new ArrayList<Sprite>();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		
+		connection = DriverManager.getConnection(connectString);
+		statement = connection.createStatement(
+				ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		ResultSet resultSet = statement.executeQuery("SELECT id, x_coord, y_coord FROM sprites");
+		
+		//After instantiating list, load from DB
+
+		while (resultSet.next()) {
+				String zid = (String) resultSet.getObject(1);
+				int zx = Integer.parseInt(resultSet.getObject(2).toString());
+				int zy = Integer.parseInt(resultSet.getObject(3).toString());
+				if ( zid.charAt(0) == 'p' ){
+					sprites.add(new Player(zid, zx, zy));
+				} else {
+					sprites.add(new Zombie(zid, zx, zy));
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
 		//May want to change this to pull directly from DB
 		String boardString = "" + sprites.size() + "&";
 		for (Sprite s: sprites) {
@@ -107,7 +134,7 @@ public class ServerBoard {
 			statement.execute("insert into sprites "
 					+ "(id, x_coord, y_coord) values "
 					+ "('"+ newZom.getId() + "'," + newZom.getX() + "," + newZom.getY() + ");");
-			
+			System.out.println("Zombie added!");
 			return true;	
 		} catch (MySQLIntegrityConstraintViolationException msicve) {
 			System.out.println("Attempted to add duplicate entry");
@@ -127,22 +154,37 @@ public class ServerBoard {
 								  @QueryParam("newX")   int finishX, 
 								  @QueryParam("newY")	int finishY) 
 	{
-		boolean found = false;
-		for (Sprite s: sprites) {
-			if (s.getClass().equals(Player.class)) {
-				if (((Player) s).getName().equals(id)){
-					found = true;
-					s.setX(finishX);
-					s.setY(finishY);
-					//Statement stmt = dbConnect.createStatement();
-					//stmt.executeQuery("select * from sprites where \"id\"=" + s.id + 
-					//					" ");
-				}
+		Player newPlay = null;
+		try {
+			newPlay = new Player(id, finishX, finishY);
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(connectString);
+			statement = connection.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			
+			statement.execute("insert into sprites "
+					+ "(id, x_coord, y_coord) values "
+					+ "('"+ newPlay.getId() + "'," + newPlay.getX() + "," + newPlay.getY() + ");");
+			
+			return getBoard();	
+		} catch (MySQLIntegrityConstraintViolationException msicve) { //Player already in database, find em and move em
+			try {
+				statement.execute("delete FROM sprites WHERE id=\"" + id + "\"");
+				statement.execute("insert into sprites "
+						+ "(id, x_coord, y_coord) values "
+						+ "('"+ newPlay.getId() + "'," + newPlay.getX() + "," + newPlay.getY() + ");");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
 			}
-		}
-		if (!found) {
-			sprites.add(new Player(id, finishX, finishY));
-		}
+			
+		} catch (SQLException se) {
+			se.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException cnfe) {
+			cnfe.printStackTrace();
+			return null;
+		} 
 		
 		Tick();
 		
@@ -150,12 +192,21 @@ public class ServerBoard {
 		return getBoard();
 	}
 	/**
-	 * Increment the tickCount variable.
-	 * Once every 60 updates, a new zombie will be added to the board.
+	 * This needs work. For starters, the tickCount static variable does not
+	 * function correctly in this context. Statelessness and all that jazz.
+	 * Not sure how I'm going to trigger this method then.
 	 * 
-	 * Also, on every tick, the zombies will update position.
+	 * Second, it will need to be reworked. The steps necessary should be:
+	 * 1) Pull all the Zombie data from database
+	 * 2) Instantiate each zombie based on DB Data
+	 * 3) Run the stalk algorithm on each, updating x, y, and target parameters
+	 * 4) Save the zombie objects back to database
+	 * 5) Return getBoard()
+	 * 
 	 */
 	private void Tick() {
+		
+		//Currently set to 10 to speed things along
 		if (++tickCount%10==0) addZombie();
 		if (tickCount >= 3600) tickCount = 0;
 		for (Sprite s : sprites) {
