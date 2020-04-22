88d2ee659cbc607ed05163e096d7b891148ed518
VacatedSum <59126285+VacatedSum@users.noreply.github.com>
2020-04-21 21:24:18 -0400

    Added MySQL DB to repo

---
+++ db/swarm/swarm.sql
@@ -0,0 +1,104 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: swarm
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `scores`
+--
+
+DROP TABLE IF EXISTS `scores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `scores` (
+  `id` varchar(10) NOT NULL,
+  `score` decimal(10,0) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scores`
+--
+
+LOCK TABLES `scores` WRITE;
+/*!40000 ALTER TABLE `scores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `scores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sprites`
+--
+
+DROP TABLE IF EXISTS `sprites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sprites` (
+  `id` varchar(10) NOT NULL,
+  `x_coord` decimal(4,0) DEFAULT '0',
+  `y_coord` decimal(4,0) DEFAULT '0',
+  `speed` int DEFAULT NULL,
+  `randomness` int DEFAULT NULL,
+  `target_x` decimal(10,0) DEFAULT NULL,
+  `target_y` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sprites`
+--
+
+LOCK TABLES `sprites` WRITE;
+/*!40000 ALTER TABLE `sprites` DISABLE KEYS */;
+INSERT INTO `sprites` VALUES ('p1040',640,500,NULL,NULL,NULL,NULL),('p1041',660,500,NULL,NULL,NULL,NULL),('z1',9,9,5,3,640,500),('z10',14,11,NULL,NULL,640,500),('z11',9,9,NULL,NULL,640,500),('z12',9,9,NULL,NULL,640,500),('z13',9,9,NULL,NULL,640,500),('z14',13,13,NULL,NULL,640,500),('z15',21,21,NULL,NULL,640,500),('z2',9,9,NULL,NULL,640,500),('z3',21,21,NULL,NULL,640,500),('z4',13,13,NULL,NULL,640,500),('z5',11,11,NULL,NULL,640,500),('z6',11,7,NULL,NULL,640,500),('z7',17,17,NULL,NULL,640,500),('z8',13,13,NULL,NULL,640,500),('z9',21,21,NULL,NULL,640,500);
+/*!40000 ALTER TABLE `sprites` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `variables`
+--
+
+DROP TABLE IF EXISTS `variables`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `variables` (
+  `idvariables` int NOT NULL AUTO_INCREMENT COMMENT 'Reserved for future use?',
+  `ticks` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idvariables`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `variables`
+--
+
+LOCK TABLES `variables` WRITE;
+/*!40000 ALTER TABLE `variables` DISABLE KEYS */;
+INSERT INTO `variables` VALUES (1,1);
+/*!40000 ALTER TABLE `variables` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-21 21:23:32
